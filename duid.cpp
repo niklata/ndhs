f53a6e4578d9d00f29c571cf324531b756245ef9
Nicholas J. Kain <nicholas@kain.us>
2020-10-29 04:32:00 -0400

    duid: Use log_* instead of fmt.

--- duid.cpp
+++ duid.cpp
@@ -1,12 +1,12 @@
 #include <cstdint>
 #include <cstring>
+#include <cstdio>
+#include <string>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <arpa/inet.h>
-#include <fmt/format.h>
-#include <fmt/printf.h>
 #include <nk/scopeguard.hpp>
 extern "C" {
 #include "nk/io.h"
@@ -20,10 +20,18 @@ char g_server_duid[g_server_duid_len];
 
 static void print_duid()
 {
-    fmt::fprintf(stderr, "DUID is '%02.x", static_cast<uint8_t>(g_server_duid[0]));
-    for (unsigned i = 1; i < g_server_duid_len; ++i)
-        fmt::fprintf(stderr, "-%02.x", static_cast<uint8_t>(g_server_duid[i]));
-    fmt::print(stderr, "'\n");
+    if (g_server_duid_len <= 0) return;
+    std::string str;
+    str.append("DUID is '");
+    char tbuf[16] = {0};
+    snprintf(tbuf, sizeof tbuf, "%.2hhx", static_cast<uint8_t>(g_server_duid[0]));
+    str.append(tbuf);
+    for (unsigned i = 1; i < g_server_duid_len; ++i) {
+        snprintf(tbuf, sizeof tbuf, "-%.2hhx", static_cast<uint8_t>(g_server_duid[i]));
+        str.append(tbuf);
+    }
+    str.append("'");
+    log_line("%s", str.c_str());
 }
 
 // Use DUID-UUID (RFC6355)
@@ -54,7 +62,7 @@ void duid_load_from_file()
 {
     const auto fd = open(DUID_PATH, O_RDONLY, 0);
     if (fd < 0) {
-        fmt::print(stderr, "No DUID found.  Generating a DUID.\n");
+        log_line("No DUID found.  Generating a DUID.");
         generate_duid();
         return;
     }
@@ -62,7 +70,7 @@ void duid_load_from_file()
     const auto r = safe_read(fd, g_server_duid, g_server_duid_len);
     if (r < 0) suicide("%s: failed to read duid from %s", __func__, DUID_PATH);
     if (r != g_server_duid_len) {
-        fmt::print(stderr, "DUID is too short to be valid.  Generating a new DUID.\n");
+        log_line("DUID is too short to be valid.  Generating a new DUID.");
         generate_duid();
         return;
     }
