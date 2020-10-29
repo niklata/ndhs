a36c6385db32059d52764a0c0c4b224d22a8660a
Nicholas J. Kain <nicholas@kain.us>
2020-10-29 04:06:39 -0400

    ndhs: Use log_* instead of fmt.

--- ndhs.cpp
+++ ndhs.cpp
@@ -50,7 +50,6 @@
 #include <signal.h>
 #include <poll.h>
 #include <errno.h>
-#include <fmt/format.h>
 #include <nk/optionarg.hpp>
 #include <nk/from_string.hpp>
 extern "C" {
@@ -86,14 +85,14 @@ static void init_listeners()
             v6l->emplace_back(std::make_unique<D6Listener>());
             if (!v6l->back()->init(i, preference)) {
                 v6l->pop_back();
-                fmt::print(stderr, "Can't bind to v6 interface: {}\n", i);
+                log_warning("Can't bind to v6 interface: %s", i.c_str());
             }
         }
         if (use_v4) {
             v4l->emplace_back(std::make_unique<D4Listener>());
             if (!v4l->back()->init(i)) {
                 v4l->pop_back();
-                fmt::print(stderr, "Can't bind to v4 interface: {}\n", i);
+                log_warning("Can't bind to v4 interface: %s", i.c_str());
             }
         }
     });
@@ -109,10 +108,8 @@ int64_t get_current_ts()
 
 void set_user_runas(size_t /* linenum */, std::string &&username)
 {
-    if (nk_uidgidbyname(username.c_str(), &ndhs_uid, &ndhs_gid)) {
-        fmt::print(stderr, "invalid user '{}' specified\n", username);
-        std::exit(EXIT_FAILURE);
-    }
+    if (nk_uidgidbyname(username.c_str(), &ndhs_uid, &ndhs_gid))
+        suicide("invalid user '%s' specified", username.c_str());
 }
 void set_chroot_path(size_t /* linenum */, std::string &&path)
 {
@@ -162,27 +159,27 @@ static void setup_signals_ndhs()
 
 static void print_version(void)
 {
-    fmt::print(stderr, "ndhs " NDHS_VERSION ", ipv6 router advertisment and dhcp server.\n"
-               "Copyright 2014-2017 Nicholas J. Kain\n"
-               "All rights reserved.\n\n"
-               "Redistribution and use in source and binary forms, with or without\n"
-               "modification, are permitted provided that the following conditions are met:\n\n"
-               "- Redistributions of source code must retain the above copyright notice,\n"
-               "  this list of conditions and the following disclaimer.\n"
-               "- Redistributions in binary form must reproduce the above copyright notice,\n"
-               "  this list of conditions and the following disclaimer in the documentation\n"
-               "  and/or other materials provided with the distribution.\n\n"
-               "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"\n"
-               "AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n"
-               "IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE\n"
-               "ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE\n"
-               "LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR\n"
-               "CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF\n"
-               "SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS\n"
-               "INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN\n"
-               "CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)\n"
-               "ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE\n"
-               "POSSIBILITY OF SUCH DAMAGE.\n");
+    log_line("ndhs " NDHS_VERSION ", ipv6 router advertisment and dhcp server.\n"
+             "Copyright 2014-2020 Nicholas J. Kain\n"
+             "All rights reserved.\n\n"
+             "Redistribution and use in source and binary forms, with or without\n"
+             "modification, are permitted provided that the following conditions are met:\n\n"
+             "- Redistributions of source code must retain the above copyright notice,\n"
+             "  this list of conditions and the following disclaimer.\n"
+             "- Redistributions in binary form must reproduce the above copyright notice,\n"
+             "  this list of conditions and the following disclaimer in the documentation\n"
+             "  and/or other materials provided with the distribution.\n\n"
+             "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"\n"
+             "AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n"
+             "IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE\n"
+             "ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE\n"
+             "LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR\n"
+             "CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF\n"
+             "SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS\n"
+             "INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN\n"
+             "CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)\n"
+             "ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE\n"
+             "POSSIBILITY OF SUCH DAMAGE.\n");
 }
 
 enum OpIdx {
@@ -245,18 +242,12 @@ static void process_options(int ac, char *av[])
     for (int i = 0; i < parse.nonOptionsCount(); ++i)
         parse_config(parse.nonOption(i));
 
-    if (!bound_interfaces_count()) {
-        fmt::print(stderr, "No interfaces have been bound\n");
-        std::exit(EXIT_FAILURE);
-    }
-    if (!ndhs_uid || !ndhs_gid) {
-        fmt::print(stderr, "No non-root user account is specified.\n");
-        std::exit(EXIT_FAILURE);
-    }
-    if (chroot_path.empty()) {
-        fmt::print(stderr, "No chroot path is specified.\n");
-        std::exit(EXIT_FAILURE);
-    }
+    if (!bound_interfaces_count())
+        suicide("No interfaces have been bound");
+    if (!ndhs_uid || !ndhs_gid)
+        suicide("No non-root user account is specified.");
+    if (chroot_path.empty())
+        suicide("No chroot path is specified.");
 
     nl_socket = std::make_unique<NLSocket>();
     init_listeners();
