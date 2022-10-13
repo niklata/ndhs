ae02b56046f0c4697eb96bcba5247adb6c030b3f
Nicholas J. Kain <nicholas@kain.us>
2022-10-13 08:48:36 -0400

    nk/prng: Use sfc64 instead of gjrand.

--- nk/prng.hpp
+++ nk/prng.hpp
@@ -11,11 +11,6 @@ extern "C" {
 
 namespace nk::rng {
 
-// This is an implementation of David Blackman's GJrand PRNG:
-// https://gjrand.sourceforge.net
-// It is structurally similar to Bob Jenkin's JSF, but performs more mixing
-// and guards against short cycles rigorously by using a Weyl sequence (s_[3]).
-
 namespace detail {
     static constexpr inline uint64_t rotl(const uint64_t x, int k) noexcept {
         return (x << k) | (x >> (64 - k));
@@ -28,44 +23,41 @@ namespace detail {
     }
 }
 
-struct gjrand64 final
+struct sfc64 final
 {
     typedef std::uint64_t result_type;
-    constexpr gjrand64(uint64_t a, uint64_t b = 0) noexcept : s_{ a, b, 2000001, 0 } { discard(14); }
-    gjrand64() : s_{ detail::nk_get_hwrng_v<uint64_t>(), detail::nk_get_hwrng_v<uint64_t>(), 2000001, 0 } { discard(14); }
+    constexpr sfc64(uint64_t a) : s_{ a, a, a, 1 } { discard(12); }
+    constexpr sfc64(uint64_t a, uint64_t b, uint64_t c) : s_{ a, b, c, 1 } { discard(12); }
+    sfc64() : s_{ 0, 0, 0, 1 } { nk_hwrng_bytes(s_, sizeof s_[0] * 3); discard(12); }
 
     constexpr std::tuple<uint64_t, uint64_t, uint64_t, uint64_t> save() const { return std::make_tuple(s_[0], s_[1], s_[2], s_[3]); }
-    constexpr void load(uint64_t a, uint64_t b, uint64_t c, uint64_t d) noexcept { s_[0] = a; s_[1] = b; s_[2] = c; s_[3] = d; }
-    constexpr inline uint64_t operator()() noexcept
+    constexpr void load(uint64_t a, uint64_t b, uint64_t c, uint64_t d) { s_[0] = a; s_[1] = b; s_[2] = c; s_[3] = d; }
+    // Modified to use a Weyl generator for the counter.
+    // This generator is also faster.
+    constexpr inline uint64_t operator()()
     {
-        s_[1] += s_[2];
-        s_[0] = detail::rotl(s_[0], 32);
-        s_[2] ^= s_[1];
-        s_[3] += 0x55aa96a5;
-        s_[0] += s_[1];
-        s_[2] = detail::rotl(s_[2], 23);
-        s_[1] ^= s_[0];
-        s_[0] += s_[2];
-        s_[1] = detail::rotl(s_[1], 19);
-        s_[2] += s_[0];
-        s_[1] += s_[3];
-        return s_[0];
+        const auto t = (s_[0] + s_[1]) ^ s_[3];
+        s_[3] += 0x6a09e667a7541669ull;
+        s_[0] = s_[1] ^ (s_[1] >> 11);
+        s_[1] = s_[2] + (s_[2] << 3);
+        s_[2] = detail::rotl(s_[2], 24) + t;
+        return t;
     }
-    constexpr void discard(size_t z) noexcept { while (z-- > 0) operator()(); }
-    static constexpr uint64_t min() noexcept { return std::numeric_limits<uint64_t>::min(); }
-    static constexpr uint64_t max() noexcept { return std::numeric_limits<uint64_t>::max(); }
+    constexpr void discard(size_t z) { while (z-- > 0) operator()(); }
+    static constexpr uint64_t min() { return std::numeric_limits<uint64_t>::min(); }
+    static constexpr uint64_t max() { return std::numeric_limits<uint64_t>::max(); }
     static constexpr size_t state_size = sizeof(uint64_t) * 4;
-    friend constexpr bool operator==(const gjrand64 &a, const gjrand64 &b) noexcept;
-    friend constexpr bool operator!=(const gjrand64 &a, const gjrand64 &b) noexcept;
+    friend constexpr bool operator==(const sfc64 &a, const sfc64 &b);
+    friend constexpr bool operator!=(const sfc64 &a, const sfc64 &b);
 private:
     uint64_t s_[4];
 };
-inline constexpr bool operator==(const gjrand64 &a, const gjrand64 &b) noexcept {
+inline constexpr bool operator==(const sfc64 &a, const sfc64 &b) {
     return a.s_[0] == b.s_[0] && a.s_[1] == b.s_[1] && a.s_[2] == b.s_[2] && a.s_[3] == b.s_[3];
 }
-inline constexpr bool operator!=(const gjrand64 &a, const gjrand64 &b) noexcept { return !operator==(a, b); }
+inline constexpr bool operator!=(const sfc64 &a, const sfc64 &b) { return !operator==(a, b); }
 
-using prng = gjrand64;
+using prng = sfc64;
 
 }
 
