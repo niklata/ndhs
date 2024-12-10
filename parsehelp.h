33a4e1f6a99c552647a92f20ac7065f0f38241c9
Nicholas J. Kain <nicholas@kain.us>
2024-12-10 09:42:36 -0500

    Don't use <ctype.h> for tolower()
    It's locale-dependent.

--- parsehelp.h
+++ parsehelp.h
@@ -1,11 +1,15 @@
 #ifndef NDHS_PARSEHELP_H_
 #define NDHS_PARSEHELP_H_
 
-#include <ctype.h>
+static inline char parsehelp_lc(char c)
+{
+    if (c >= 'A' && c <= 'Z') return 'a' + (c - 'A');
+    return c;
+}
 
 static inline void lc_string_inplace(char *s, size_t len)
 {
-    for (size_t i = 0; i < len; ++i) s[i] = tolower(s[i]);
+    for (size_t i = 0; i < len; ++i) s[i] = parsehelp_lc(s[i]);
 }
 
 static void assign_strbuf(char *dest, size_t *destlen, size_t maxlen, const char *start, const char *end)
